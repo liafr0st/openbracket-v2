5191b7a138f5ba90598c30fe816fb601799e415e
Talia Hurrell <44178314+taliafr0st@users.noreply.github.com>
2024-08-08 17:11:01 +0100

    landing page

---
+++ svelte/src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<div class="flex flex-row max-w-xl md:mx-auto mb-72">
+    <div id="mainimg" class="h-1/6 w-full py-4">
+        <img class="object-contain mx-auto" src="\textmark-white.png" alt="OpenBracket Textmark">
+    </div>
+</div>
+<div class="flex flex-row max-w-xl md:mx-auto my-3.5 items-center justify-center">
+    <button class="text-4xl text-ob-gray hover:text-white bg-white border-4 border-white hover:bg-ob-gray h-18 w-480px px-4 py-2 mx-12 my-auto rounded-full">Create Tournament</button>
+</div>
+<div class="flex flex-row max-w-xl md:mx-auto my-3.5 items-center justify-center">
+    <button class="text-4xl text-white hover:text-ob-gray bg-ob-gray border-4 border-white hover:bg-white h-18 w-480px px-4 py-2 mx-12 my-auto rounded-full">Import Tournament</button>
+</div>
+
+
+<style lang="postcss">
+    :global(html) {
+      background-color: #333333;
+    }
+    :global(div) {
+        font-family: 'Bahnschrift';
+    }
+</style>
