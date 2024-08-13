93907ee86e4ac9f0edf33036ec6a2294af96d4d3
Talia Hurrell <44178314+taliafr0st@users.noreply.github.com>
2024-08-13 18:41:57 +0100

    css fixes + view & admin sidebar

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -4,17 +4,26 @@
     </div>
 </div>
 <div class="flex flex-row max-w-xl md:mx-auto mt-72 my-3.5 items-center justify-center">
-    <a class="btn text-4xl leading-[52px] text-ob-gray text-center align-middle bg-white border-4 border-white hover:bg-opacity-90 h-18 w-480px px-4 py-2 mx-12 rounded-full" href="/create">Create Tournament</a>
+    <a 
+        class="btn text-4xl leading-[52px] text-ob-gray text-center align-middle bg-white border-4 border-white hover:bg-opacity-90 h-18 w-480px px-4 py-2 mx-12 rounded-full" href="/create">
+        Create Tournament
+    </a>
 </div>
 <div class="flex flex-row max-w-xl md:mx-auto my-3.5 items-center justify-center">
-    <a class="btn text-4xl leading-[52px] text-white text-center align-middle bg-ob-gray border-4 border-white hover:bg-white hover:bg-opacity-10 h-18 w-480px px-4 py-2 mx-12 rounded-full" href="/import">Import Tournament</a>
+    <a 
+        class="btn text-4xl leading-[52px] text-white text-center align-middle bg-ob-gray border-4 border-white hover:bg-white hover:bg-opacity-10 h-18 w-480px px-4 py-2 mx-12 rounded-full"
+        href="/import">
+        Import Tournament
+    </a>
 </div>
 
-<style lang="postcss">
-    :global(html) {
-      background-color: #333333;
-    }
-    :global(div) {
-        font-family: 'Bahnschrift';
-    }
-</style>
+<svelte:head>
+    <style>
+        :root {
+            background-color: #333333;
+        }
+        div {
+            font-family: 'Bahnschrift';
+        }
+    </style>
+</svelte:head>
--- svelte/src/routes/create/+page.svelte
+++ svelte/src/routes/create/+page.svelte
@@ -19,20 +19,31 @@
     </div>
 </div>
 <div class="flex flex-row max-w-xl md:mx-auto my-3.5 items-center justify-center">
-    <button id="publish" class="text-4xl leading-[52px] text-ob-gray text-center align-middle bg-white border-4 border-white hover:bg-opacity-90 h-18 w-480px px-4 py-2 mx-12 rounded-full" on:click={() => publish()}>Publish Tournament</button>
+    <button 
+        id="publish" 
+        class="text-4xl leading-[52px] text-ob-gray text-center align-middle bg-white border-4 border-white hover:bg-opacity-90 h-18 w-480px px-4 py-2 mx-12 rounded-full"
+        on:click={() => publish()}>
+        Publish Tournament
+    </button>
 </div>
 <div class="flex flex-row max-w-xl md:mx-auto my-3.5 items-center justify-center">
-    <a class="btn text-4xl leading-[52px] text-white text-center align-middle bg-ob-gray border-4 border-white hover:bg-white hover:bg-opacity-10 h-18 w-480px px-4 py-2 mx-12 rounded-full" href="/">Go Back</a>
+    <a 
+        class="btn text-4xl leading-[52px] text-white text-center align-middle bg-ob-gray border-4 border-white hover:bg-white hover:bg-opacity-10 h-18 w-480px px-4 py-2 mx-12 rounded-full" 
+        href="/">
+        Go Back
+    </a>
 </div>
 
-<style lang="postcss">
-    :global(html) {
-      background-color: #333333;
-    }
-    :global(div) {
-        font-family: 'Bahnschrift';
-    }
-</style>
+<svelte:head>
+    <style>
+        :root {
+            background-color: #333333;
+        }
+        div {
+            font-family: 'Bahnschrift';
+        }
+    </style>
+</svelte:head>
 
 <script>
     import cfg from "../../../obconfig.json";
